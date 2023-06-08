e65e585a6aebcf3a4e7392d9002f2de9eace3688
ornel77 <t.ornel@yahoo.fr>
2023-06-09 01:15:21 +0200

    navbar ok

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+
+function Home() {
+  return (
+    <div className='w-full h-screen bg-[#0A192F]'>Home</div>
+  )
+}
+
+export default Home
