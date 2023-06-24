d8761c714c13d82023b5ac500409f579558cbd74
ornel77 <t.ornel@yahoo.fr>
2023-06-24 12:27:13 +0200

    remove sides file and add hover on the links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,27 +26,27 @@ function Navbar() {
 
       <ul className='hidden md:flex'>
         <li>
-          <Link to='home' smooth={true} duration={500}>
+          <Link to='home' smooth={true} duration={500} className="hover:text-pink-600 duration-300">
             Home
           </Link>
         </li>
         <li>
-          <Link to='about' smooth={true} duration={500}>
+          <Link to='about' smooth={true} duration={500} className="hover:text-pink-600 duration-300">
             About
           </Link>
         </li>
         <li>
-          <Link to='skills' smooth={true} duration={500}>
+          <Link to='skills' smooth={true} duration={500} className="hover:text-pink-600 duration-300">
             Skills
           </Link>
         </li>
         <li>
-          <Link to='projects' smooth={true} duration={500}>
+          <Link to='projects' smooth={true} duration={500} className="hover:text-pink-600 duration-300">
             Projects
           </Link>
         </li>
         <li>
-          <Link to='contact' smooth={true} duration={500}>
+          <Link to='contact' smooth={true} duration={500} className="hover:text-pink-600 duration-300">
             Contact
           </Link>
         </li>
